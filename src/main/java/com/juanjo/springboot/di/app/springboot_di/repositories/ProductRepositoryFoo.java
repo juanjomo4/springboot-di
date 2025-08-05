65bda5da6db59bbec8407d959cf595f5ad10e72f
Juanjo M. <60217633+juanjomo4@users.noreply.github.com>
2025-08-05 10:13:50 +0200

    Uso de implentacion de una misma interfaz en varios repositorios con Primary

---
+++ src/main/java/com/juanjo/springboot/di/app/springboot_di/repositories/ProductRepositoryFoo.java
@@ -0,0 +1,25 @@
+package com.juanjo.springboot.di.app.springboot_di.repositories;
+
+import java.util.Collections;
+import java.util.List;
+
+// import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import com.juanjo.springboot.di.app.springboot_di.models.Product;
+
+// @Primary
+@Repository
+public class ProductRepositoryFoo implements ProductRepository {
+
+    @Override
+    public List<Product> findAll() {
+        return Collections.singletonList(new Product(1L, "Monitor Asus 27 pulgadas", 600L));
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return new Product(1L, "Monitor Asus 27 pulgadas", 600L);
+    }
+
+}
